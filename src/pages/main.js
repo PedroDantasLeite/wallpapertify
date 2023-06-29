f33354834d2d7cc2e777169f652262836526d455
PedroDantasLeite <pedro.leite@mobilicidade.com.br>
2023-06-29 10:24:53 -0300

    Gika project started

--- src/pages/main.js
+++ src/pages/main.js
@@ -5,10 +5,15 @@ import "./main.css";
 import { Bars3Icon } from "@heroicons/react/20/solid";
 import Pedro from "./styles/pedro";
 import { Container } from "react-bootstrap";
+import Gika from "./styles/gika";
 
 export default function Main() {
   const [nowPlaying, setNowPlaying] = useState({});
+  const [dropdownVisible, setDropodownVisible] = useState(false);
+  const [style, setStyle] = useState("Pedro");
   const spotifyApi = new SpotifyWebApi();
+
+  const estilos = ["Pedro", "Gika", "Algm"];
 
   useEffect(() => {
     if (!localStorage.getItem("refreshToken")) {
@@ -23,6 +28,25 @@ export default function Main() {
   }, [spotifyApi]);
 
   useEffect(() => {
+    const getNowPlaying = () => {
+      spotifyApi.getMyCurrentPlaybackState().then(
+        (response) => {
+          setNowPlaying({
+            name: response.item.name,
+            artist: response.item.artists[0].name,
+            albumArt: response.item.album.images[0].url,
+            length: response.item.duration_ms,
+            currentTime: response.progress_ms,
+          });
+        },
+        () => {
+          getAccessToken(localStorage.getItem("refreshToken")).then((data) => {
+            spotifyApi.setAccessToken(data);
+            getNowPlaying();
+          });
+        }
+      );
+    };
     const updateSong = () => {
       const interval = setInterval(() => {
         getNowPlaying();
@@ -30,41 +54,47 @@ export default function Main() {
       return () => clearInterval(interval);
     };
     updateSong();
-  }, []);
+  }, [spotifyApi]);
 
-  const getNowPlaying = () => {
-    spotifyApi.getMyCurrentPlaybackState().then(
-      (response) => {
-        setNowPlaying({
-          name: response.item.name,
-          artist: response.item.artists[0].name,
-          albumArt: response.item.album.images[0].url,
-          length: response.item.duration_ms,
-          currentTime: response.progress_ms,
-        });
-      },
-      () => {
-        getAccessToken(localStorage.getItem("refreshToken")).then((data) => {
-          spotifyApi.setAccessToken(data);
-          getNowPlaying();
-        });
-      }
-    );
+  const renderStyle = () => {
+    if (!nowPlaying.name) return <h3>waiting song..</h3>;
+    switch (style) {
+      case "Pedro":
+        return <Pedro nowPlaying={nowPlaying} />;
+      case "Gika":
+        return <Gika nowPlaying={nowPlaying} />;
+      default:
+        return <h3>How?</h3>;
+    }
   };
 
   return (
     <>
-      {(nowPlaying.name && (
-        <>
-          <Container
-            type="button"
-            className="w-auto h-auto position-absolute left-0 top-0 m-4 cursor-pointer"
-          >
-            <Bars3Icon width={35} stroke="white" stroke-width="0.7" />
-          </Container>
-          <Pedro nowPlaying={nowPlaying} />
-        </>
-      )) || <h3>waiting song..</h3>}
+      <Container className="w-auto h-auto position-absolute left-0 top-0 m-4">
+        <span className="cursor-pointer w-auto h-auto" type="button">
+          <Bars3Icon
+            onClick={() => setDropodownVisible(!dropdownVisible)}
+            width={35}
+            stroke="white"
+            stroke-width="0.6"
+          />
+        </span>
+        {dropdownVisible && (
+          <div className="text-center w-max h-auto mt-2 bg-white rounded">
+            {estilos.map((pessoa) => (
+              <div
+                type="button"
+                className="w-max py-1 mx-4 cursor-pointe color bg-gray-300"
+                onClick={() => setStyle(pessoa)}
+                key={pessoa}
+              >
+                {pessoa}
+              </div>
+            ))}
+          </div>
+        )}
+      </Container>
+      {renderStyle()}
     </>
   );
 }
--- src/pages/styles/gika.js
+++ src/pages/styles/gika.js
@@ -1,5 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import chroma from "chroma-js";
 
-export default function Pika() {
-  return <div>gika</div>;
+export default function Gika({ nowPlaying }) {
+  const [colors, setColors] = useState([]);
+
+  document.body.style.backgroundColor = colors[0];
+
+  useEffect(() => {
+    let image = document.getElementById("image");
+    setColors(chroma.scale().colors(5));
+  }, []);
+
+  return (
+    <>
+      <img src={nowPlaying.albumArt} className="img" alt="Image" id="Image" />
+    </>
+  );
 }
